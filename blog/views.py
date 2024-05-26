0affb5d5d61c7f5168cfb09162d87250f0767a18
LUKA SEKINE <luka.sekine@gmail.com>
2024-05-26 15:20:21 +0900

    fvb-cbv-basicを実装
    fvb-cbv-basicを実装

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 def index(request):
     #TOP画面を表示する関数
     return render(request, 'blog/index.html')
+
+class IndexView(TemplateView):
+    #TOP画面を表示するクラス
+    template_name = 'blog/index.html'
