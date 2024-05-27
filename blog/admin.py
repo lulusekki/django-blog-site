0f4cbc0ba357cd8c03acb8ad768015048c925f0f
LUKA SEKINE <luka.sekine@gmail.com>
2024-05-28 07:25:53 +0900

    adminsite-customize
    More items displayed on the admin site's blog listing page.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from blog.models import Category, Blog
 
-# Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display=('id','title','postdate','category')
+
+admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
