4c6f5554c3bb7a51cab2faba77a24f58b296e97f
LUKA SEKINE <luka.sekine@gmail.com>
2024-05-28 06:19:45 +0900

    categorytable_and_blogtable_create
    categoyテーブルとblogテーブルの作成

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
     'blog.apps.BlogConfig', 
 ]
 
